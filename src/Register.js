70e154e19d0a170f93f1c1d0e11f0a52f2698683
Ibon <776561@unizar.es>
2021-03-24 10:50:36 +0100

    Register bien

--- src/Register.js
+++ src/Register.js
@@ -1,87 +1,38 @@
 
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React,{Component} from 'react';
 
-class Register extends React.component{
-    constructor(props) {
-        super(props);
 
-        this.state = {
-            user: {
-                username: '',
-                password: '',
-                email: '',
-                preg: '',
-                res:''
-            },
-            submitted: false
-        };
-
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
-
-    handleChange(event) {
-        const { name, value } = event.target;
-        const { user } = this.state;
-        this.setState({
-            user: {
-                ...user,
-                [name]: value
-            }
-        });
-    }
-
-    handleSubmit(event) {
-        event.preventDefault();
-
-        this.setState({ submitted: true });
-        const { user } = this.state;
-        if (user.username && user.password && user.email && user.preg && user.res) {
-            this.props.register(user);
-        }
-    }
-
-    render() {
-        const { user, submitted } = this.state;
+class Register extends React.Component{
+        render() {
+        
         return (
             <div className="col-md-6 col-md-offset-3">
                 <h2>Register</h2>
                 <form name="form" onSubmit={this.handleSubmit}>
-                    <div className={'form-group' + (submitted && !user.username ? ' has-error' : '')}>
+                    <div className={'form-group' }>
                         <label htmlFor="firstName">UserName</label>
-                        <input type="text" className="form-control" name="username" value={user.username} onChange={this.handleChange} />
-                        {submitted && !user.username &&
-                        <div className="help-block">UserName is required</div>
-                        }
+                        <input type="text" className="form-control" name="username"  />
+                        
                     </div>
-                    <div className={'form-group' + (submitted && !user.password ? ' has-error' : '')}>
+                    <div className={'form-group' }>
                         <label htmlFor="password">Password</label>
-                        <input type="password" className="form-control" name="password" value={user.password} onChange={this.handleChange} />
-                        {submitted && !user.password &&
-                        <div className="help-block">Password is required</div>
-                        }
+                        <input type="password" className="form-control" name="password"  />
+                        
                     </div>
-                    <div className={'form-group' + (submitted && !user.email ? ' has-error' : '')}>
+                    <div className={'form-group' }>
                         <label htmlFor="email">Email</label>
-                        <input type="text" className="form-control" name="email" value={user.email} onChange={this.handleChange} />
-                        {submitted && !user.email &&
-                        <div className="help-block">Email is required</div>
-                        }
+                        <input type="text" className="form-control" name="email"  />
+                        
                     </div>
-                    <div className={'form-group' + (submitted && !user.preg ? ' has-error' : '')}>
+                    <div className={'form-group' }>
                         <label htmlFor="preg">Preg</label>
-                        <input type="text" className="form-control" name="preg" value={user.preg} onChange={this.handleChange} />
-                        {submitted && !user.preg &&
-                        <div className="help-block">Preg is required</div>
-                        }
+                        <input type="text" className="form-control" name="preg"  />
+                        
                     </div>
-                    <div className={'form-group' + (submitted && !user.res ? ' has-error' : '')}>
+                    <div className={'form-group' }>
                         <label htmlFor="res">Res</label>
-                        <input type="text" className="form-control" name="res" value={user.res} onChange={this.handleChange} />
-                        {submitted && !user.res &&
-                        <div className="help-block">Res is required</div>
-                        }
+                        <input type="text" className="form-control" name="res"  />
+                        
                     </div>
                     <div className="form-group">
                         <button className="btn btn-primary">Register</button>
