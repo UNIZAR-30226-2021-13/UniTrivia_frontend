07ddedacf37fb329943c294bdfd53e4679e194f9
oscargo240 <778572@unizar.es>
2021-04-19 17:33:41 +0200

    cambios con socket.io en room.js

--- src/game/room.js
+++ src/game/room.js
@@ -1,27 +1,52 @@
-import React, {useEffect, useState} from 'react'
+import React, {useEffect} from 'react'
 import {Card, CardContent, TextField} from '@material-ui/core';
-//import {io} from "socket.io-client";
+import {io,socketIOClient} from "socket.io-client";
+import {getToken} from "../Utils/Common";
 
-const ENDPOINT = "http://unitrivia.herokuapp.com";
+const http = require("http");
+
+const ENDPOINT = "http://localhost:3000/";
 
 
 function Room() {
-    const [response, setResponse] = useState("");
 
-    /*useEffect(() => {
-        const socket = io(ENDPOINT);
-        //const socket = socketIOClient(ENDPOINT);
-        socket.on("crearSala", data => {
-            setResponse(data);
-            //console.log(data)
+    useEffect(() => {
+        /*const socket = io('http://localhost:3000',{path: '/api/partida'});
+        socket.on('connect',()=>{
+
+        })*/
+       // const socket = socketIOClient(ENDPOINT);
+
+        const conn = io("http://localhost:3000/api/partida", {
+            withCredentials: true,
+            extraHeaders: {
+                jwt: getToken(),
+                operacion: "crearSala",
+                priv: "true"
+            }
         });
-    }, []);*/
+        conn.on("connect", () => {
+            conn.emit("mensaje", "Hola Mundo");
+        })
+        console.log(conn.id)
+        console.log('hola')
+
+        //const socket = socketIOClient(ENDPOINT);
+        //const socket = socketIOClient(ENDPOINT);
+
+        /*socket.on('connect',()=>{
+            console.log(socket.id)
+
+            console.log('hola')
+        });*/
+
+    }, []);
 
     return (
         <Card>
             <CardContent>
 
-                <TextField value={'Esto es la sala'}>
+                <TextField>
 
                 </TextField>
 
