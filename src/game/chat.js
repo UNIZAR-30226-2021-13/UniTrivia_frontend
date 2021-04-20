1423592745f2e854e2007ce24c61bfe48eb382ce
DiegoBielsa <776564@unizar.es>
2021-04-20 21:07:25 +0200

    Chat hecho, debería funcionar, depurar cuando funciones crear/unirse a sala

--- src/game/chat.js
+++ src/game/chat.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { Widget, addResponseMessage  } from 'react-chat-widget';
 import 'react-chat-widget/lib/styles.css';
+import {setUserSession,getToken,removeUserSession} from "../Utils/Common";
 
 const io = require("socket.io-client");
 const http = require("http");
@@ -8,27 +9,36 @@ const http = require("http");
 
 
 
-const ENDPOINT = "http://unitrivia.herokuapp.com/api/partida";
+const ENDPOINT = "http://localhost:3000/api/partida";
 
 
 
 function Chat(props){
+    let conn = props.socket;
 
-    let conn = undefined;
-    let conexion = undefined;
+    /*let connTest1 = io(ENDPOINT,{
+        extraHeaders:{
+            jwt: getToken(),
+            operacion: "crearSala",
+            priv: "true"
+        }
+    });*/
 
-    useEffect(() => {
-        conn = io(ENDPOINT);
-        conn.on("connect");
-    }, []);
+    conn.on('chat', ({user, msg})=>{
+        let msgFull = '(' + user + ')' + msg;
+        addResponseMessage(msgFull);
+    })
+
 
     const handleNewUserMessage = (newMessage) => {
         console.log(`New message incomig! ${newMessage}`);
         // Now send the message throught the backend API
-        conn.emit("mensaje", "Hola a todos")
+        conn.emit("mensaje", {newMessage});
         //end backend
-        addResponseMessage("que tal");
+        //addResponseMessage("que tal");
     }
+
+
 
     return (
         <div className="Chat">
