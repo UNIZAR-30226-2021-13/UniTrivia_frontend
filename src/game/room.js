49d0f493b78cb737331de4296d3dc68bc9afc5c1
ibon2 <ibonlkz@gmail.com>
2021-04-28 23:28:26 +0200

    Codigo Sala sacado por pantalla

--- src/game/room.js
+++ src/game/room.js
@@ -1,25 +1,59 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useState} from 'react'
 import {Card, CardContent, TextField} from '@material-ui/core';
-import {io,socketIOClient} from "socket.io-client";
+//import {io,socketIOClient} from "socket.io-client";
 import {getToken} from "../Utils/Common";
 
+import {conn} from "../Play";
+
+const io = require("socket.io-client");
 const http = require("http");
 
 const ENDPOINT = "http://localhost:3000/api/partida";
 
 
-function Room() {
+function Room(props) {
+    let [jugadores,setJugadores] = useState(null);
+    let [codigoSala,setCodigoSala] = useState(null);
+    const listarJugadores = () => {
+        /*console.log("antes de listar");
+        conn.on('cargarJugadores',(users)=>{
+            console.log(users);
+            setJugadores(users);
+        })
+        console.log(jugadores);*/
+        console.log("Hola estoy en listar");
+    }
+    const devolverCodigoSala = () => {
 
+
+        conn.emit("obtenerIdSala",(id)=>{
+            //console.log("En obtener id ");
+            console.assert(id!=='','Error al obtener idSala');
+            setCodigoSala(id);
+            console.log(id);
+        })
+
+
+        console.log("El identificador es :");
+        return (
+            <h1>El codigo de la sala es {codigoSala}</h1>
+        )
+    }
+    const botonEmpezar = (newMessage) => {
+
+    }
     return (
-        <Card>
-            <CardContent>
-
-                <TextField>
-
-                </TextField>
-
-            </CardContent>
-        </Card>
+        <div>
+            <div>
+                Hola
+            </div>
+            <div>
+                {listarJugadores()}
+            </div>
+            <div>
+                {devolverCodigoSala()}
+            </div>
+        </div>
 
     );
 }
