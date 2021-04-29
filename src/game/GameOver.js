8a0116bac9e1982ce5988a82f3d5702cc478e291
DiegoBielsa <71701253+DiegoBielsa@users.noreply.github.com>
2021-04-30 00:14:30 +0200

    Añadido gameOver

--- src/game/GameOver.js
+++ src/game/GameOver.js
@@ -1,1 +1,39 @@
 
+import styled from 'styled-components'
+import React from 'react'
+import Button from '@material-ui/core/Button';
+
+
+const Title = styled.h1`
+    margin-top: 4em;
+    font-size: 48px;
+`;
+
+const Points = styled.p`
+    font-size: 24px;
+    margin-bottom: 3em;
+`;
+
+function GameOver(props){
+    let pts = props.pts;
+    let estado;
+    if(pts === 1){
+        estado = "CORRECTA";
+    }else{
+        estado = "INCORRECTA";
+    }
+
+
+
+    return (
+        <>
+            <Title>Resultado</Title>
+            <Points>Respuesta {estado}</Points>
+
+
+        </>
+
+    )
+}
+
+export default GameOver
